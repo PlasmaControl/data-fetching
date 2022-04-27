fc037484c046daca12c06361217d1b60b80fb991
Joe Abbate <joseph.abbate7@gmail.com>
2022-04-26 20:33:19 -0700

    add script to create the set of shots, based exclusively on whether shot is plasma and greater than 120000

---
+++ dump_shots.py
@@ -0,0 +1,18 @@
+from toksearch import Pipeline
+from toksearch.sql.mssql import connect_d3drdb
+import numpy as np
+
+# from https://diii-d.gat.com/DIII-D/software/ml/toksearch/latest/SQL.html
+conn = connect_d3drdb()
+parameterized_query = """
+    select shot
+    from shots_type
+    where shot_type = 'plasma' and shot > %d
+    order by shot desc
+    """
+minimum_shot = 120000
+pipeline = Pipeline.from_sql(conn, parameterized_query, minimum_shot)
+results = pipeline.compute_serial()
+
+np.save('data/shots.npy',
+        np.array([elem['shot'] for elem in results]))
