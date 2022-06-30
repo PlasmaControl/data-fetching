d5e69a2d5aca25f26ddcb703036d38f47a0a4a80
Viraj Mehta <virajm@cs.cmu.edu>
2022-06-29 17:15:30 -0700

    added code for smarter pulling

--- get_shots_to_read.py
+++ get_shots_to_read.py
@@ -1,5 +1,8 @@
 import h5py
 from pathlib import Path
+from toksearch import Pipeline
+from toksearch.sql.mssql import connect_d3drdb
+import numpy as np
 
 data_path = Path("/cscratch/mehtav/data.h5")
 
@@ -13,9 +16,21 @@ def get_last_datapoint():
 
 def main(most_recent_shot, dest):
     last_datapoint = get_last_datapoint()
-    shots_to_get = np.array(list(range(last_datapoint + 1, most_recent_shot + 1))).astype(int)
-    # write these to dest in a numpy file
-    np.save(dest, shots_to_get)
+    # from https://diii-d.gat.com/DIII-D/software/ml/toksearch/latest/SQL.html
+    conn = connect_d3drdb()
+    parameterized_query = """
+        select shot
+        from shots_type
+        where shot_type = 'plasma' and shot > %d
+        order by shot desc
+        """
+    pipeline = Pipeline.from_sql(conn, parameterized_query, last_datapoint)
+    results = pipeline.compute_serial()
+
+    shots=np.array([elem['shot'] for elem in results])
+
+    np.save('data/shots.npy',
+            shots)
 
 if __name__ == '__main__':
     main(int(sys.argv[1]), Path(sys.argv[2]))
