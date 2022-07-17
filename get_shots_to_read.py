e46bd11eabffa68619e02561a63e809fdf9fe096
Viraj Mehta <virajm@cs.cmu.edu>
2022-07-17 08:29:38 -0700

    seems like this should work

--- get_shots_to_read.py
+++ get_shots_to_read.py
@@ -1,3 +1,4 @@
+import sys
 import h5py
 from pathlib import Path
 from toksearch import Pipeline
@@ -7,14 +8,14 @@ import numpy as np
 data_path = Path("/cscratch/mehtav/data.h5")
 
 def get_last_datapoint():
-    f = h5py.File(data_path, 'r')
+    dataset = h5py.File(data_path, 'r')
     all_dataset_shots = set(dataset.keys())
     all_dataset_shots.remove('times')
     shot_num_list = sorted([int(key) for key in all_dataset_shots])
     return shot_num_list[-1]
 
 
-def main(most_recent_shot, dest):
+def main(np_dest, txt_dest):
     last_datapoint = get_last_datapoint()
     # from https://diii-d.gat.com/DIII-D/software/ml/toksearch/latest/SQL.html
     conn = connect_d3drdb()
@@ -27,10 +28,13 @@ def main(most_recent_shot, dest):
     pipeline = Pipeline.from_sql(conn, parameterized_query, last_datapoint)
     results = pipeline.compute_serial()
 
-    shots=np.array([elem['shot'] for elem in results])
+    shot_list = [elem['shot'] for elem in results]
+    shots = np.array(shot_list)
 
-    np.save('data/shots.npy',
-            shots)
+    np.save(np_dest, shots)
+    with txt_dest.open('w') as f:
+        f.writelines([shot + '\n' for shot in shot_list])
+    print(f"wrote {len(shot_list)} new shots to {np_dest} and {txt_dest}")
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]), Path(sys.argv[2]))
+    main(Path(sys.argv[1]), Path(sys.argv[2]))
