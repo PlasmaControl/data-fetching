8fc60844dab85b8a59e03c6c1970aac0b8f96610
Viraj Mehta <virajm@cs.cmu.edu>
2022-10-24 13:06:37 -0400

    first maybe working version

--- get_control_shot_numbers.py
+++ get_control_shot_numbers.py
@@ -1,9 +1,10 @@
 import pickle
 from toksearch.sql.mssql import connect_d3drdb
+from toksearch import Pipeline
 
 def main(test_shot_path, out_path):
     with open(test_shot_path, 'r') as f:
-        test_shots = [s.strip() for s in f.readlines()]
+        test_shots = set([s.strip() for s in f.readlines()])
 
     # get all run numbers for test shots
     runs = set()
@@ -12,7 +13,22 @@ def main(test_shot_path, out_path):
                 FROM shots
                 WHERE shot IN ({','.join(test_shots)})"""
 
+    pipeline = Pipeline.from_sql(conn, query)
 
+    records = pipeline.compute_serial()
+    runs = set(records)
 
+    query2 = f"""SELECT shot, run
+                 FROM shots
+                 WHERE run IN ({','.join(runs)})"""
+    pipeline = Pipeline.from_sql(conn, query2)
+    records = pipeline.compute_serial()
+    control_shots = []
+    for shot, run in records:
+        #check if not test shot, add to list
+        if shot not in test_shots:
+            control_shots.append(shot)
 
-
+    with out_path.open('w') as f:
+        f.write('\n'.join(control_shots))
+        f.write('\n')
