4bacdbcfb3cbb74e9a9aa6f2255e8b598310456c
andybroth <rothsteina@gmail.com>
2024-04-23 16:09:02 -0700

    Fix bug on file splitting

--- new_database_maker.py
+++ new_database_maker.py
@@ -151,7 +151,11 @@ if cfg['logistics']['overwrite_shots']:
                 del final_data[str(shot)]
 
 subshots=[]
-num_files=int(len(all_shots)/(cfg['logistics']['max_shots_per_run'])) + 1
+num_files=int(len(all_shots)/(cfg['logistics']['max_shots_per_run']))
+
+if len(all_shots) % cfg['logistics']['max_shots_per_run'] != 0:
+    num_files += 1
+
 for i in range(num_files):
     subshots.append(all_shots[i*cfg['logistics']['max_shots_per_run']:min((i+1)*cfg['logistics']['max_shots_per_run'],
                                                                            len(all_shots))])
